8482d04106c47a35bf8be815fd2c6b62cd5b6ba6
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-03-16 08:43:14 +0300

    Fix sha256 hash formatting in cli.rb

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -6,17 +6,17 @@ class Cli < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.2.2/pocketenv_v0.2.2_x86_64-apple-darwin.tar.gz"
-    sha256 "sha256:5b34d10a8083397c8a67cbee41a73ce332733663e2e07ab6527817c6412b3996"
+    sha256 "5b34d10a8083397c8a67cbee41a73ce332733663e2e07ab6527817c6412b3996"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.2.2/pocketenv_v0.2.2_aarch64-apple-darwin.tar.gz"
-    sha256 "sha256:d05ee85e9b9a0ae4c66016c49715d2934bc718580e02f1f3f8140e814f6e7971"
+    sha256 "d05ee85e9b9a0ae4c66016c49715d2934bc718580e02f1f3f8140e814f6e7971"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.2.2/pocketenv_v0.2.2_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "sha256:7afffeae12c8372091f0dbf2624b5aaa898cbf72a004d4a05259986dccc19395"
+    sha256 "7afffeae12c8372091f0dbf2624b5aaa898cbf72a004d4a05259986dccc19395"
   end
 
   def install

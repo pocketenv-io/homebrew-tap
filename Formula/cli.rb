3e964ebcfce513897d331b07840f8c9b94cadce9
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-03-22 22:50:20 +0300

    Update SHA256 checksums for pocketenv releases

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -2,21 +2,21 @@ class Cli < Formula
   desc "Manage your development environment with ease ✨"
   homepage "https://github.com/pocketenv-io/pocketenv"
   url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.3.2/pocketenv_v0.3.2_aarch64-apple-darwin.tar.gz"
-  sha256 "fc09f10b1f8dc439ba10de591c4cc2689d237e7766a9386e0e578a030fd0b873"
+  sha256 "192d7c4113c0f6daf76175735634beba0afc8b719257cabc6ca2a671cf71a3fb"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.3.2/pocketenv_v0.3.2_x86_64-apple-darwin.tar.gz"
-    sha256 "8abbd9057476ca074ecde83f79be319d9f5f270164a4e547d10f985ab4ec9810"
+    sha256 "0d41765c6a8707a731b0c6fa5059d6117035382d63454368e9d39b6ad5993e16"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.3.2/pocketenv_v0.3.2_aarch64-apple-darwin.tar.gz"
-    sha256 "fc09f10b1f8dc439ba10de591c4cc2689d237e7766a9386e0e578a030fd0b873"
+    sha256 "192d7c4113c0f6daf76175735634beba0afc8b719257cabc6ca2a671cf71a3fb"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.3.2/pocketenv_v0.3.2_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "b08e2a543477d902053eb705130f14a0616c133266e7c067151cc2c2a8274e8a"
+    sha256 "93fe7e42b872c9991692926a3756297886af89b272a45c54f68926399f721753"
   end
 
   def install

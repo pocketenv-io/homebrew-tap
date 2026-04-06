5fb53efb9db0763d70adf66c765b2564f7ee146a
Tsiry Sandratraina <tsiry.sndr@pocketenv.io>
2026-04-06 16:30:11 +0300

    Update Pocketenv to version 0.6.6

--- Formula/pocketenv.rb
+++ Formula/pocketenv.rb
@@ -1,22 +1,22 @@
 class Pocketenv < Formula
   desc "Manage your development environment with ease ✨"
   homepage "https://github.com/pocketenv-io/pocketenv"
-  url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.4/pocketenv_v0.6.4_aarch64-apple-darwin.tar.gz"
-  sha256 "9ab582aae5b85083478632a83e9136fbfead9b2a2f237df1a7add2e78ca8da7b"
+  url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.6/pocketenv_v0.6.6_aarch64-apple-darwin.tar.gz"
+  sha256 "2887a8b2eae5304923613d3b17496f92b996dc58542f8ddc7c98f369f6d36d30"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.4/pocketenv_v0.6.4_x86_64-apple-darwin.tar.gz"
-    sha256 "d223b73035208e2d3ce3d5c3d97daaf980267c509b02f04db28296d8e018c141"
+    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.6/pocketenv_v0.6.6_x86_64-apple-darwin.tar.gz"
+    sha256 "013b43fa9fcb4fbc61339919eba8dc16c1ff0b2c5b65bf2734dcf1028da8b549"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.4/pocketenv_v0.6.4_aarch64-apple-darwin.tar.gz"
-    sha256 "9ab582aae5b85083478632a83e9136fbfead9b2a2f237df1a7add2e78ca8da7b"
+    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.6/pocketenv_v0.6.6_aarch64-apple-darwin.tar.gz"
+    sha256 "2887a8b2eae5304923613d3b17496f92b996dc58542f8ddc7c98f369f6d36d30"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.4/pocketenv_v0.6.4_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "e1cd30d8cbe9442b42277ebca2e361c6e5e66b5dcb072cc344a603f56c5dc7df"
+    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.6.6/pocketenv_v0.6.6_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "093200394d2c845edd0d8600d6bcbdc66da1e800b79ba419d6143a25252c7cb6"
   end
 
   def install

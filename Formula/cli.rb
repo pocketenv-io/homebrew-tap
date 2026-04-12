b695cd2f93d168c6a0bc9c0476a2eb0fe3e630d8
Tsiry Sandratraina <tsiry.sndr@pocketenv.io>
2026-04-12 13:17:36 +0300

    Update pocketenv to version 0.7.1

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,22 +1,22 @@
 class Cli < Formula
   desc "Manage your development environment with ease ✨"
   homepage "https://github.com/pocketenv-io/pocketenv"
-  url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.0/pocketenv_v0.7.0_aarch64-apple-darwin.tar.gz"
-  sha256 "48041f31cdd8231277ba74edc7f9a7dbc5ef4a30e38c80e5bb4449d5c7984c5d"
+  url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.1/pocketenv_v0.7.1_aarch64-apple-darwin.tar.gz"
+  sha256 "6531d03e3c273cdcc20c08f99c9f2fc1204ae6438048f8e0c8dbc749c3878e88"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.0/pocketenv_v0.7.0_x86_64-apple-darwin.tar.gz"
-    sha256 "1cfa3eddfc121d99b538411b2f4a5990f3a7cc835c0a89c45d70af800c2db1ae"
+    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.1/pocketenv_v0.7.1_x86_64-apple-darwin.tar.gz"
+    sha256 "7a59756010b4401ccf118617335d84d0b03899782ed0d414d58d154f669b5522"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.0/pocketenv_v0.7.0_aarch64-apple-darwin.tar.gz"
-    sha256 "48041f31cdd8231277ba74edc7f9a7dbc5ef4a30e38c80e5bb4449d5c7984c5d"
+    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.1/pocketenv_v0.7.1_aarch64-apple-darwin.tar.gz"
+    sha256 "6531d03e3c273cdcc20c08f99c9f2fc1204ae6438048f8e0c8dbc749c3878e88"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.0/pocketenv_v0.7.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "3ed0cc6e7f6f2340cb88c2e179b961b2c624bf135303b3243d1385e4e55314fb"
+    url "https://github.com/pocketenv-io/pocketenv/releases/download/v0.7.1/pocketenv_v0.7.1_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "89aab66b7ad7b2210d69f42eede31400592883a06c018fb0b81f5ac698be314b"
   end
 
   def install
